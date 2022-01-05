6e4a365425e19b382222a10b5c6ce907b4572788
Daniel Gomes <danieflam3@gmail.com>
2022-01-05 12:03:26 -0300

    Graficos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "componentes/BarChart";
 import DataTable from "componentes/DataTable";
+import DonutChart from "componentes/DonutChart";
 import Footer from "componentes/footer";
 import NavBar from "componentes/navbar";
 
@@ -7,7 +9,22 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Ola Mundo</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
 
         <DataTable />
       </div>
